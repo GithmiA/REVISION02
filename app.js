671d1c2ac658866551d958d88f7b641dd2f2ec01
GithmiA <githmianuradha67@gmail.com>
2026-08-06 16:55:54 +0530

    Refactor: Comment out product fetching and rendering code; add customer object to localStorage

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
-fetch('https://fakestoreapi.com/products')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
+// fetch('https://fakestoreapi.com/products')
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data)
    
-let productContainner = document.getElementById("productItem");
-let body = "";
-data.forEach(element => {
-    body += `
-        <div class="col">
-                    <div class="card shadow-sm">
-                    <img src="${element.image}" alt="" class="card-img-top">
-                        <div class="card-body">
-                        <h3>${element.title}</h3>
-                            <p class="card-text">${element.description}</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group"> <button type="button"
-                                        class="btn btn-sm btn-outline-secondary">${"Buy Now "}${element.price}</button> <button type="button"
-                                        class="btn btn-sm btn-outline-secondary">${element.rating.rate}</button> </div> <small
-                                    class="text-body-secondary">9 mins</small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-    `
-});
-productContainner.innerHTML = body;
-})
+// let productContainner = document.getElementById("productItem");
+// let body = "";
+// data.forEach(element => {
+//     body += `
+//         <div class="col">
+//                     <div class="card shadow-sm">
+//                     <img src="${element.image}" alt="" class="card-img-top">
+//                         <div class="card-body">
+//                         <h3>${element.title}</h3>
+//                             <p class="card-text">${element.description}</p>
+//                             <div class="d-flex justify-content-between align-items-center">
+//                                 <div class="btn-group"> <button type="button"
+//                                         class="btn btn-sm btn-outline-secondary">${"Buy Now "}${element.price}</button> <button type="button"
+//                                         class="btn btn-sm btn-outline-secondary">${element.rating.rate}</button> </div> <small
+//                                     class="text-body-secondary">9 mins</small>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//     `
+// });
+// productContainner.innerHTML = body;
+// })
+let customer = {
+    name: "John Doe",
+    age:21,
+    city: "New York"
+};
+localStorage.setItem("customer",JSON.stringify(customer));
+console.log(JSON.parse(localStorage.getItem("customer")));
+console.log(customer.age);
