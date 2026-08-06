fad50cf113c0a879211e75932ddbf8f34b1baefe
GithmiA <githmianuradha67@gmail.com>
2026-08-06 12:22:46 +0530

    Add product display functionality and Bootstrap styling

---
+++ app.js
@@ -0,0 +1,28 @@
+fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+   
+let productContainner = document.getElementById("productItem");
+let body = "";
+data.forEach(element => {
+    body += `
+        <div class="col">
+                    <div class="card shadow-sm">
+                    <img src="${element.image}" alt="" class="card-img-top">
+                        <div class="card-body">
+                        <h3>${element.title}</h3>
+                            <p class="card-text">${element.description}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group"> <button type="button"
+                                        class="btn btn-sm btn-outline-secondary">${"Buy Now "}${element.price}</button> <button type="button"
+                                        class="btn btn-sm btn-outline-secondary">${element.rating.rate}</button> </div> <small
+                                    class="text-body-secondary">9 mins</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    `
+});
+productContainner.innerHTML = body;
+})
