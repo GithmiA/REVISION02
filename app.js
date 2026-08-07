08f4a8d0e04f9dad31247835da856a33e11f3507
GithmiA <githmianuradha67@gmail.com>
2026-08-07 13:04:42 +0530

    Implement customer management functionality: add, search, delete, update, and clear storage features

--- app.js
+++ app.js
@@ -26,11 +26,61 @@
 // });
 // productContainner.innerHTML = body;
 // })
-let customer = {
-    name: "John Doe",
-    age:21,
-    city: "New York"
-};
-localStorage.setItem("customer",JSON.stringify(customer));
-console.log(JSON.parse(localStorage.getItem("customer")));
-console.log(customer.age);
+// let customer = {
+//     name: "John Doe",
+//     age:21,
+//     city: "New York"
+// };
+// localStorage.setItem("customer",JSON.stringify(customer));
+// console.log(JSON.parse(localStorage.getItem("customer")));
+// console.log(customer.age);
+
+const customerList = [];
+
+function btnAddCustomerOnAction(){
+   let customer = {
+    id:document.getElementById("txtCustomerId").value,
+    name:document.getElementById("txtCustomerName").value,
+    city:document.getElementById("txtCustomerCity").value,
+    salary:document.getElementById("txtCustomerSalary").value
+   }
+   customerList.push(customer);
+   localStorage.setItem("customer",JSON.stringify(customerList));
+   console.log(JSON.parse(localStorage.getItem("customer")));
+
+   
+}
+
+function btnSearchCustomerOnAction(){
+    let customerList = JSON.parse(localStorage.getItem("customer"));
+
+    let customer = customerList.find(customer => {
+        return customer.id === document.getElementById("txtCustomerId").value
+    });
+
+    document.getElementById("txtCustomerName").value = customer.name;
+    document.getElementById("txtCustomerCity").value = customer.city;
+    document.getElementById("txtCustomerSalary").value = customer.salary;
+}
+function btnDeleteCustomerOnAction(){
+    let customerList = JSON.parse(localStorage.getItem("customer"));
+
+    let customer = customerList.findIndex(customer => {
+        return customer.id === document.getElementById("txtCustomerId").value
+    });
+
+    console.log(customer);
+    customerList.splice(customer, 1);
+    localStorage.setItem("customer",JSON.stringify(customerList));
+    console.log(customerList);
+    
+}
+function btnUpdateCustomerOnAction(){
+    
+}
+function btnClearStorageOnAction(){
+    localStorage.clear("customer");
+}
+function btnLoadTablerOnAction(){
+    
+}
