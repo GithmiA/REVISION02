f90669b4a7bbf02941f21940482c963b04e0d35b
GithmiA <githmianuradha67@gmail.com>
2026-08-08 13:52:37 +0530

    Refactor: Comment out customer management functions and add weather app functionality

--- app.js
+++ app.js
@@ -35,52 +35,79 @@
 // console.log(JSON.parse(localStorage.getItem("customer")));
 // console.log(customer.age);
 
-const customerList = [];
+// const customerList = [];
 
-function btnAddCustomerOnAction(){
-   let customer = {
-    id:document.getElementById("txtCustomerId").value,
-    name:document.getElementById("txtCustomerName").value,
-    city:document.getElementById("txtCustomerCity").value,
-    salary:document.getElementById("txtCustomerSalary").value
-   }
-   customerList.push(customer);
-   localStorage.setItem("customer",JSON.stringify(customerList));
-   console.log(JSON.parse(localStorage.getItem("customer")));
+// function btnAddCustomerOnAction(){
+//    let customer = {
+//     id:document.getElementById("txtCustomerId").value,
+//     name:document.getElementById("txtCustomerName").value,
+//     city:document.getElementById("txtCustomerCity").value,
+//     salary:document.getElementById("txtCustomerSalary").value
+//    }
+//    customerList.push(customer);
+//    localStorage.setItem("customer",JSON.stringify(customerList));
+//    console.log(JSON.parse(localStorage.getItem("customer")));
 
    
-}
+// }
 
-function btnSearchCustomerOnAction(){
-    let customerList = JSON.parse(localStorage.getItem("customer"));
+// function btnSearchCustomerOnAction(){
+//     let customerList = JSON.parse(localStorage.getItem("customer"));
 
-    let customer = customerList.find(customer => {
-        return customer.id === document.getElementById("txtCustomerId").value
-    });
+//     let customer = customerList.find(customer => {
+//         return customer.id === document.getElementById("txtCustomerId").value
+//     });
 
-    document.getElementById("txtCustomerName").value = customer.name;
-    document.getElementById("txtCustomerCity").value = customer.city;
-    document.getElementById("txtCustomerSalary").value = customer.salary;
-}
-function btnDeleteCustomerOnAction(){
-    let customerList = JSON.parse(localStorage.getItem("customer"));
+//     document.getElementById("txtCustomerName").value = customer.name;
+//     document.getElementById("txtCustomerCity").value = customer.city;
+//     document.getElementById("txtCustomerSalary").value = customer.salary;
+// }
+// function btnDeleteCustomerOnAction(){
+//     let customerList = JSON.parse(localStorage.getItem("customer"));
 
-    let customer = customerList.findIndex(customer => {
-        return customer.id === document.getElementById("txtCustomerId").value
-    });
+//     let customer = customerList.findIndex(customer => {
+//         return customer.id === document.getElementById("txtCustomerId").value
+//     });
 
-    console.log(customer);
-    customerList.splice(customer, 1);
-    localStorage.setItem("customer",JSON.stringify(customerList));
-    console.log(customerList);
+//     console.log(customer);
+//     customerList.splice(customer, 1);
+//     localStorage.setItem("customer",JSON.stringify(customerList));
+//     console.log(customerList);
     
-}
-function btnUpdateCustomerOnAction(){
+// }
+// function btnUpdateCustomerOnAction(){
     
-}
-function btnClearStorageOnAction(){
-    localStorage.clear("customer");
-}
-function btnLoadTablerOnAction(){
+// }
+// function btnClearStorageOnAction(){
+//     localStorage.clear("customer");
+// }
+// function btnLoadTablerOnAction(){
     
+// }
+
+
+
+function btnGetWeatherOnAction(){
+const apikey = "f66783e9bf0e43c29b070720262507";
+const baseUrl = "http://api.weatherapi.com/v1";
+
+    let loction = document.getElementById("txtLocation").value;
+fetch(`${baseUrl}/current.json?key=${apikey}&q=${loction}`).then(res=>res.json()).then(data=>{
+    //console.log(data)
+
+document.getElementById("weatherInfo").innerHTML =`
+           <h2>${data.location.name}</h2>
+           <img src="${data.current.condition.icon}" alt="">
+           <p>${data.current.condition.text}°C</p>
+           <p>${data.current.temp_c}°C</p>
+
+`
+})
 }
+navigator.geolocation.getCurrentPosition((position=>{
+    console.log(position);
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+}))
+console.log();
+
